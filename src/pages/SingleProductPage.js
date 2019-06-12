9a362bee5bea6c996e5bd04414b113e1728e0a3e
Sid Narla <sridhar.narla@thameswater.co.uk>
2019-06-12 15:50:18 +0100

    changed price icon

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -41,7 +41,7 @@ export default function SingleProductPage() {
                       company : {company}
                     </h5>
                     <h5 className="text-main text-capitalize mb-4">
-                      price : ${price}
+                      price : £{price}
                     </h5>
                     <p className="text-capitalize text-title mt-3">
                       some info about product :
